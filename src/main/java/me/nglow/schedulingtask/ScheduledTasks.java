5491066034ae1361db61334c6c59ae2b0d6ce81d
nglow <nglow90@gmail.com>
2021-08-04 14:14:37 +0900

    Initial commit

---
+++ src/main/java/me/nglow/schedulingtask/ScheduledTasks.java
@@ -0,0 +1,34 @@
+package me.nglow.schedulingtask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000) // Set start time when task is started.
+    public void reportCurrentTimeByFixedRate() {
+        log.info("The time recorded by fixed rate: {}", LocalDateTime.now().format(format));
+    }
+
+    @Scheduled(fixedDelay = 3000) // Set start time when task is done.
+    public void reportCurrentTimeFixedDelay() {
+        log.info("The time recorded by fixed delay: {}", LocalDateTime.now().format(format));
+    }
+
+    @Scheduled(cron = "* * * * * ?")
+    public void reportCurrentTimeCron() {
+        log.info("The time recorded by cron: {}", LocalDateTime.now().format(format));
+    }
+}
---
+++ src/test/java/me/nglow/schedulingtask/SchedulingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nglow.schedulingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
